317c04763767eaeb4955f0590cdcc46022234ddc
Boris Petrov <boris_petrov@live.com>
2018-12-09 18:42:57 +0200

    Add example code

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import Mixin from '@ember/object/mixin';
+import MutableArray from '@ember/array/mutable';
+import Observable from '@ember/object/observable';
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
@@ -10,5 +13,19 @@ const App = Application.extend({
 });
 
 loadInitializers(App, config.modulePrefix);
+
+const properties = {
+  replace(index, amount, objects = []) {
+    console.log('replace called!');
+    return this;
+  },
+};
+
+const result = Mixin
+  .create(MutableArray, Observable, properties)
+  .without(['length'])
+  .apply([]);
+
+result.pushObject(123);
 
 export default App;
